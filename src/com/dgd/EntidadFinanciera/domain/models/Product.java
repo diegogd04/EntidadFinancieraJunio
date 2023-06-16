1c3c8255322b263ff7fdd7d5f32f253014b5b29e
diegogd04 <diego2612004@gmail.com>
2023-06-16 16:55:14 +0200

    Crear clase Product

---
+++ src/com/dgd/EntidadFinanciera/domain/models/Product.java
@@ -0,0 +1,31 @@
+package com.dgd.EntidadFinanciera.domain.models;
+
+public class Product {
+    protected Integer codigo;
+    protected String nombre;
+    protected String descripcion;
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}

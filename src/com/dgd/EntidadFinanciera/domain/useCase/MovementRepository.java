2bd21fd79812e75b467b7fc1d9aa90fddebeef54
diegogd04 <diego2612004@gmail.com>
2023-06-16 17:58:37 +0200

    Crear interfaz MovementRepository para casos de uso de Movement

---
+++ src/com/dgd/EntidadFinanciera/domain/useCase/MovementRepository.java
@@ -0,0 +1,15 @@
+package com.dgd.EntidadFinanciera.domain.useCase;
+
+import com.dgd.EntidadFinanciera.domain.models.Movement;
+
+import java.util.List;
+
+public interface MovementRepository {
+    public void save(Movement movement);
+
+    public Movement getByCodigo();
+
+    public List<Movement> getAll();
+
+    List<Movement> getAllWithremote();
+}

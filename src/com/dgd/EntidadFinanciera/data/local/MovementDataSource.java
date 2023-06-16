c74c4a690b5a248b4ae269ecff3671ddcf8ca8c7
diegogd04 <diego2612004@gmail.com>
2023-06-16 18:20:59 +0200

    Crear MovementDataSource

---
+++ src/com/dgd/EntidadFinanciera/data/local/MovementDataSource.java
@@ -0,0 +1,15 @@
+package com.dgd.EntidadFinanciera.data.local;
+
+import com.dgd.EntidadFinanciera.domain.models.Movement;
+
+import java.util.List;
+
+public interface MovementDataSource {
+    public void save(Movement movement);
+
+    public Movement getByCodigo(Integer movementCodigo);
+
+    public List<Movement> getAll();
+
+    List<Movement> getAllWithremote();
+}

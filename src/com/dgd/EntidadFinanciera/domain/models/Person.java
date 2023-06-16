40800db592d77936952bea5954c0a4bf09b0c049
diegogd04 <diego2612004@gmail.com>
2023-06-16 16:47:58 +0200

    Crear clase Person

---
+++ src/com/dgd/EntidadFinanciera/domain/models/Person.java
@@ -0,0 +1,31 @@
+package com.dgd.EntidadFinanciera.domain.models;
+
+public class Person {
+    protected String dni;
+    protected String nombre;
+    protected String apellidos;
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+}
